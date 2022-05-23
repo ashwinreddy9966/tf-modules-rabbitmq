b440449bed81ab183a559a2a991dd1ea8054408e
manu <manu@local.com>
2022-05-23 23:46:57 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,40 +1,42 @@
-resource "aws_db_instance" "mysql" {
-  identifier             = "roboshop-mysql-${var.ENV}"
-  allocated_storage      = 10
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t3.micro"
-  username               = "admin1"
-  password               = "RoboShop1"
-  parameter_group_name   = "default.mysql5.7"
-  skip_final_snapshot    = true
-  db_subnet_group_name   = aws_db_subnet_group.mysql.name
-  vpc_security_group_ids = [aws_security_group.mysql.id]
-}
 
-resource "aws_db_subnet_group" "mysql" {
-    name       = "roboshop-mysql-${var.ENV}"
-    subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
-  tags = {
-    Name = "My DB subnet group"
-  }
-}
+#resource "aws_db_instance" "mysql" {
+#  identifier             = "roboshop-mysql-${var.ENV}"
+#  allocated_storage      = 10
+#  engine                 = "mysql"
+#  engine_version         = "5.7"
+#  instance_class         = "db.t3.micro"
+#  username               = "admin1"
+#  password               = "RoboShop1"
+#  parameter_group_name   = "default.mysql5.7"
+#  skip_final_snapshot    = true
+#  db_subnet_group_name   = aws_db_subnet_group.mysql.name
+#  vpc_security_group_ids = [aws_security_group.mysql.id]
+#}
+#
+#resource "aws_db_subnet_group" "mysql" {
+#    name       = "roboshop-mysql-${var.ENV}"
+#    subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+#
+#  tags = {
+#    Name = "My DB subnet group"
+#  }
+#}
+#
+#resource "aws_db_parameter_group" "mysql" {
+#  name   = "roboshop-mysql-${var.ENV}"
+#  family = "mysql5.7"
+#}
 
-resource "aws_db_parameter_group" "mysql" {
-  name   = "roboshop-mysql-${var.ENV}"
-  family = "mysql5.7"
-}
-
-resource "aws_security_group" "mysql" {
-  name        = "roboshop-mysql-${var.ENV}"
-  description = "roboshop-mysql-${var.ENV}"
+resource "aws_security_group" "rabbitmq" {
+  name        = "roboshop-rabbitmq-${var.ENV}"
+  description = "roboshop-rabbitmq-${var.ENV}"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
 
   ingress {
-    description = "Allows MySQL Port"
-    from_port   = 3306
-    to_port     = 3306
+    description = "Allows rabbitmq Port"
+    from_port   = 5672
+    to_port     = 5672
     protocol    = "tcp"
     cidr_blocks = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
   }
