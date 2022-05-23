bd73defc09e9f766473514f7c88708a05ceaeee5
manu <manu@local.com>
2022-05-23 23:53:34 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -12,20 +12,6 @@ resource "aws_mq_broker" "rabbitmq" {
   }
 }
 
-#resource "aws_db_instance" "mysql" {
-#  identifier             = "roboshop-mysql-${var.ENV}"
-#  allocated_storage      = 10
-#  engine                 = "mysql"
-#  engine_version         = "5.7"
-#  instance_class         = "db.t3.micro"
-#  username               = "admin1"
-#  password               = "RoboShop1"
-#  parameter_group_name   = "default.mysql5.7"
-#  skip_final_snapshot    = true
-#  db_subnet_group_name   = aws_db_subnet_group.mysql.name
-#  vpc_security_group_ids = [aws_security_group.mysql.id]
-#}
-#
 #resource "aws_db_subnet_group" "mysql" {
 #    name       = "roboshop-mysql-${var.ENV}"
 #    subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
