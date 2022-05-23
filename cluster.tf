e7702c9ffe13aa5c69bf9491190ec8d191572d7b
manu <manu@local.com>
2022-05-23 23:48:05 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,11 +1,6 @@
 resource "aws_mq_broker" "rabbitmq" {
   broker_name = "example"
-
-  configuration {
-    id       = aws_mq_configuration.test.id
-    revision = aws_mq_configuration.test.latest_revision
-  }
-
+  
   engine_type        = "ActiveMQ"
   engine_version     = "5.15.9"
   host_instance_type = "mq.t2.micro"
