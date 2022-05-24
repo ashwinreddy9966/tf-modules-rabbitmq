dfcc4cfec89691ac22d5879c2d2d05f899545038
manu <manu@local.com>
2022-05-24 10:20:40 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,10 +1,7 @@
-#resource "aws_route53_record" "record" {
-#  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
-#  name    = "rabbitmq-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
-#  type    = "CNAME"
-#  ttl     = "60"
-#  records = [aws_mq_broker.aws_mq_broker.????]
-#}
-output "rabbitmq" {
-  value = aws_mq_broker.rabbitmq
+resource "aws_route53_record" "record" {
+  zone_id = data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ID
+  name    = "rabbitmq-${var.ENV}.${data.terraform_remote_state.vpc.outputs.HOSTEDZONE_PRIVATE_ZONE}"
+  type    = "CNAME"
+  ttl     = "60"
+  records = [aws_mq_broker.aws_mq_broker.????]
 }
