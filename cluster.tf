47ef313b27b42455c6379f73ec475a3b241376c3
manu <manu@local.com>
2022-05-27 10:03:06 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -18,7 +18,7 @@ resource "aws_spot_instance_request" "rabbitmq" {
   instance_type           = var.RABBITMQ_INSTANCE_TYPE
   wait_for_fulfillment    = true
   vpc_security_group_ids  = [aws_security_group.rabbitmq.id]
-  subnet_id               = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+  subnet_id               = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS[0]
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
